c754a2f49edec995f2d7a6270ad77550092ddf3d
chi-touch <gloebudav@gmail.com>
2024-08-08 00:52:52 +0100

    navbar

---
+++ connect/src/pages/Home/section1/section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section1 = () => {
+    return (
+        <div>
+            <div>
+                <a>Explore by category</a>
+            </div>
+        </div>
+    );
+};
+
+export default Section1;
